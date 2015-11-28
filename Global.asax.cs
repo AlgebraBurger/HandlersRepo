4eb3c8f88d6513514ada9c781d2c6d4000937aed
juliusbacosa <juliusbacosa@gmail.com>
2015-11-28 20:12:56 +0800

    - commit

---
+++ Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Handlers
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        public Global()
+        { 
+            MapRequestHandler += HandleEvent;
+            PostMapRequestHandler += HandleEvent;
+            PreRequestHandlerExecute += HandleEvent;
+            PostRequestHandlerExecute += HandleEvent;
+        }
+        private void HandleEvent(object sender, EventArgs e)
+        {
+            string eventType = Context.CurrentNotification.ToString();
+            if (Context.IsPostNotification)
+            {
+                eventType = "Post" + eventType;
+            }
+            System.Diagnostics.Debug.WriteLine("Request Event: {0}",new[] { eventType });
+        }
+    }
+}
